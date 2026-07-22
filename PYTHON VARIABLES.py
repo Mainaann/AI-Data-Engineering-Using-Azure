a7ced9ff44be4ccdcc0526e80109144cafde0131
Ann Maina <141801101+Mainaann@users.noreply.github.com>
2026-07-21 21:16:48 -0500

    Add files via upload
    This is a python script on conditional statement using python

---
+++ PYTHON VARIABLES.py
@@ -0,0 +1,47 @@
+#PYTHON VARIABLES - A VARIABLE STORE DATA
+#THE PRINT COMMAND
+print ('Hello data Engineers')
+#ASSIGNING VARIABLE TO VALUES
+name = 'Hapinness'
+age = 25
+print(name)
+print(age)
+#CREATE A VARIABLE CALL NAME AND ASSIGN THE VALUE CALLED MARY TO IT
+name = 'Mary'
+#CREATE A VARIABLE CALLED COUNTRY AND ASSIGN A VALUE AMERICA TO IT
+country = 'America'
+#CREATE A VARIABLE COURSE AND ASSIGN A VALUE DATA ENGINEERING TO IT
+course = 'Data Engineering'
+print(name)
+print(country)
+print(course)
+#CHECK DATA TYPE OF A VALUE IN A VARIABLE
+print(type(name))
+print(type(country))
+print(type(course))
+#CHANGING DATA TYPES
+x = str(5)
+y = int(5)
+z = float(5)
+print(x)
+print(y)
+print(z)
+#BASIC MATH OPERATIONS
+a = 10
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+#MORE MATH OPERATIONS
+x = 25
+y = 10
+z = 16
+print(x + y / z)
+print(y - z)
+print(z * x)
+print(x % z) #MODULUS
+print(y // z) #FLOOR DIVISION
+print(x ** 2) #EXPONENT
+print((x + y) / z)
+print(x // y) #FLOOR DIVISION
